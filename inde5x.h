c4acbeba0a38fc60f8e3340c0c84b7018f8e93fc
dmitriynaumov1024 <naumov1024@gmail.com>
2020-09-09 13:47:34 +0300

    start of new life

--- inde5x.h
+++
@@ -1,9 +0,0 @@
-<body>
-<p>
-Hello world!
-</p>
-<a href='page1.html'>Go to page1.html</a><br>
-<a href='page2.html'>Go to page2.html (We HaVe PhP tHeRe)</a><br>
-<a href='page3.php'>Go to page3.php (We HaVe PhP tHeRe)</a><br>
-
-</body>
